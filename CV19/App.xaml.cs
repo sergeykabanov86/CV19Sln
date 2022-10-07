52449a2eeffbfd07a4436b90e2ad9252a3d6d2da
Sergey Kabanov <sergeykabanov86@gmail.com>
2022-10-07 14:08:05 +0200

    Добавьте файлы проекта.

---
+++ CV19/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CV19
+{
+  
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public bool IsDesignMode { get; set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            IsDesignMode = false;
+            base.OnStartup(e);  
+        }
+    }
+}
